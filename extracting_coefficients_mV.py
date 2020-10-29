a0dfe01b1a759d938b6429fd59eae201c912a824
Dmitry <PanshinDA@yandex.ru>
2020-10-29 22:33:20 +0500

    Рассчитаны и обновлены С-коэффициенты(от 11,361 до 19,739 мВ) для ТП типа R.

--- extracting_coefficients_mV.py
+++ extracting_coefficients_mV.py
@@ -2,7 +2,7 @@
 import numpy as np
 
 # массив температур для требуемых ТП
-temperature = np.array([t for t in range(-200, 1, 1)])
+temperature = np.array([t for t in range(1064, 1665, 1)])
 
 # массив значений температур в mV
 with open("EDS_mV.txt") as file:
@@ -11,7 +11,7 @@ t_in_mV = np.array(sorted(map(float, s.split())))
 print(len(t_in_mV))
 
 # количество коэффициентов
-q_k = 8
+q_k = 5
 
 # извлечение коэффициентов
 k_poly = np.polyfit(t_in_mV, temperature, q_k)
@@ -27,7 +27,7 @@ def calc_poly(x, a):
 
 
 # получение температуры из mV
-mV = 41.276
+mV = 18.849
 t_target = calc_poly(mV, k_poly)
 
 print(mV, '<—>', t_target)
